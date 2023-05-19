49a15d8de02f5313d5476f777d553d52a118eb9f
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-05-19 20:15:29 +0200

    chore: setup project

---
+++ src/MartenDotNetTestTemplate.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace MartenDotNetTestTemplate.Tests;
+
+public class UnitTest1 : IAsyncLifetime
+{
+  [Fact]
+  public void Test1()
+  {
+  }
+
+  public Task InitializeAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task DisposeAsync()
+  {
+    throw new NotImplementedException();
+  }
+}
